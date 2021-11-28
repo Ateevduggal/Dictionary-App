ebb71ffe369a17fa71e8c44336f30cf3a26cd69f
AteevDuggal <akd3257@gmail.com>
2021-11-28 09:49:40 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
           <div className="col-12 text-center fw-bold fs-1 p-3 bg-primary text-white">
             Pocket Dictonary
           </div>
-          <div className="form-floating bg-primary py-3 pb-5 text-center">
+          <div className="form-floating bg-primary py-3 pb-5 d-flex justify-content-center">
             <input
               type="text"
               className="form-control-sm border-0 px-2 col-3"
@@ -43,8 +43,13 @@ const App = () => {
               id="floatingInput"
               value={word}
               onChange={(e) => setWord(e.target.value)}
-              onKeyPress = {Search}
             />
+            <button
+              className="btn btn-dark text-light col-md-1 col-sm-2 mx-2"
+              onClick={Search}
+            >
+              Saerch
+            </button>
           </div>
         </div>
       </div>
